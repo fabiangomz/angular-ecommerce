85238bc82f53e4781beb8d2f4cdc7c437eeb8d44
Fabián Gómez Campo <fgomezc@uxcale.com>
2025-07-07 12:57:16 +0200

    feat: project initialization

---
+++ src/app/store/components/navbar/navbar.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'store-navbar',
+  imports: [],
+  templateUrl: './navbar.component.html',
+})
+export class NavbarComponent {}
---
+++ src/app/store/layouts/store-layout/store-layout.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
+
+@Component({
+  selector: 'app-store-layout',
+  imports: [RouterOutlet, NavbarComponent],
+  templateUrl: './store-layout.component.html',
+})
+export class StoreLayoutComponent {}
